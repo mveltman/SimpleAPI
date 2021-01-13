23302800bd2a91d2ab7b4800d474d0ca82d215e3
equentiz <equentiz@hotmail.com>
2021-01-13 12:13:12 +0100

    initial commit

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        public WeatherForecastController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Freezing";
+        }
+    }
+}
---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetWeather(){
+            var returnvalue = controller.Get();
+            Assert.Equal("Freezing", returnvalue);
+        }
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
